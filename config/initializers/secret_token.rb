4d7b6039a3339646c6a03f2a28c554879aedc6a5
Veer Yuganter Singh <vysingh@amazon.com>
2014-03-03 15:10:00 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstAppAptana::Application.config.secret_key_base = '6b37f6db2f7a5a96bb8d02bcb3e089ee1f6181cd66b106714de250740808042347f9c3b4fe624ddd82aa910322f89996dfd2d03a0bfe6ae1f4238a6ae281d7d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FirstAppAptana::Application.config.session_store :cookie_store, key: '_first_app_aptana_session'
